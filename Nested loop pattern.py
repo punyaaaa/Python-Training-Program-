5a31382eb3d4b1123e13a9eb2934de877e80a005
Punya Y Poojary <157906238+punyaaaa@users.noreply.github.com>
2025-07-23 23:07:27 +0530

    Add files via upload

---
+++ Nested loop pattern.py
@@ -0,0 +1,5 @@
+a=10
+for i in range(6):
+    for j in range(i+1):
+        a=a+2
+print(a)
---
+++ Pyramid.py
@@ -0,0 +1,6 @@
+
+a=int(input("Enter num of rows "))
+for i in range(a):
+    for i in range(a-i):
+        print("*",end=" ")
+    print()    
